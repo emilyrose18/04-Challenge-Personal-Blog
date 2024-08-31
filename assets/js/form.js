473d43104b9982b8648d3549fb822d5fc852a9fc
emilyrose18 <emilyramirez218@gmail.com>
2024-08-31 18:12:42 -0400

    finshed the for validation function and the store function to store the forms values into localstorage in the form.js file

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,49 @@
 const submitEl = document.querySelector('#submit');
-function submitButton(event) {
-  let x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
+let x = document.querySelector('#uname');
+let y = document.querySelector('#title');
+let z = document.querySelector('#content');
+
+function formValidation(event) {
+  if (x.value == '' || y.value == '' || z.value == '') {
+    alert('Name must be filled out');
+    return;
+  } else {
+    event.preventDefault()
+    store();
+  };
+};
+
+submitEl.addEventListener('click', formValidation);
+
+
+let posts = [];
+
+function getPost(key) {
+  if (localStorage.getItem(key) == null) {
+    console.log('what');
+    return posts;
+  } else{
+  return JSON.parse(localStorage.getItem(key)); }
 }
-submitEl.addEventListener('click', submitButton);
+
+function addPost(key, item) {
+  let currentPost = getPost(key);
+  console.log('hi');
+  currentPost.push(item);
+  localStorage.setItem(key, JSON.stringify(currentPost));
+  return currentPost;
+}
+
+function store() {
+  let post  = 
+    {  
+      'Username' : x.value, 
+      'title' : y.value,
+      'content' : z.value
+
+    }
+    console.log('hello');
+    addPost('allPosts', post);
+
+  };
+
